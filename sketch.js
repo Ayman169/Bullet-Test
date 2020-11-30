ad91772c2194a4a4d575f3e3ea920ede9b6b2f83
Sayeda Ayman <74244510+Ayman169@users.noreply.github.com>
2020-11-30 10:54:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,14 +4,14 @@ var speed,weight,bullet;
 function setup() {
   createCanvas(1600,400);
 
+  speed= random(223,321);
+  weight= random(30,52);
+  thickness = random(22,83);
+  
   bullet = createSprite(50,200,50,10);
   bullet.shapeColor = "white";
   wall = createSprite(1200,200,thickness,height/2);
   wall.shapeColor = "brown";
-
-  speed= random(223,321);
-  weight= random(30,52);
-  thickness = random(22,83);
 
   bullet.velocityX = speed;
 
